ab7ada8f3fd44792cf4b921415489e290c4ae9fa
Mayureshwar S <mayur20032004@gmail.com>
2026-05-07 18:24:31 +0530

    last commit

--- src/pages/PageNotFound/PageNotFound.jsx
+++ src/pages/PageNotFound/PageNotFound.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const PageNotFound = () => {
   return (
-    <div>
-      <h1>Page Not Found</h1>
+    <div className='bg-black flex itmes-center justify-center'>
+      <h1 className='font-extrabold text-white text-5xl'>Page Not Found</h1>
     </div>
   )
 }
